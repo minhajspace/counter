c6b509eb953e8c90f5bc3859e4af20fb89a7d3d1
minhajspace <minhaj.space@gmail.com>
2020-01-02 20:06:13 +0530

    complete

--- src/counter.js
+++ src/counter.js
@@ -14,7 +14,7 @@ class Counter extends React.Component {
 
     }
     onResetClick = () => {
-        this.setState({ counter: this.state.counter = 0 })
+        this.setState({ counter: 0 })
 
     }
 
