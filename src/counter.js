6b27d8dceca509296b1456c42f91b03987f97c5f
minhajspace <minhaj.space@gmail.com>
2020-01-02 15:42:07 +0530

    addling counter logic

--- src/counter.js
+++ src/counter.js
@@ -4,20 +4,30 @@ class Counter extends React.Component {
     state = {
         counter: 0
     }
+
+    onIncrementClick = () => {
+        this.setState({ counter: this.state.counter + 1 })
+        console.log(this.state.counter)
+    }
+    onDecrementClick = () => {
+        this.setState({ counter: this.state.counter - 1 })
+        console.log(this.state.counter)
+    }
+
     render() {
         return (
             <div className="ui cards">
                 <div className="card">
                     <div className="content">
+                    </div>
 
 
 
+                    <div className="extra content">
+                        <div className="ui two buttons">
+                            <div className="ui basic green button" onClick={this.onIncrementClick}>Increment</div>
+                            <div className="ui basic red button" onClick={this.onDecrementClick}>Decrement</div>
 
-                        <div class="extra content">
-                            <div class="ui two buttons">
-                                <div class="ui basic green button">Increment</div>
-                                <div class="ui basic red button">Decrement</div>
-                            </div>
                         </div>
                     </div>
                 </div>
